2b0d26efcf20586185af3b8012213369d92f9439
Bob Robey <brobey@lanl.gov>
2020-02-03 15:06:19 -0700

    adding scratch directory for trinitite

--- jobtest.sh
+++ jobtest.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -l
-#SBATCH -N 1
+#SBATCH -N 8
 #SBATCH -J hdf5test
 
-dsize=$(( 10**6 ))
-for ((inc=0; i<7; i++))
+cd /lustre/ttscratch1/brobey
+dsize=$(( 10**9 ))
+#for ((inc=0; i<7; i++))
+for ((nodes=1; nodes<8; nodes++))
 do
-   runstring="srun -n 64 ./hdf5proxy -size $dsize -nb_files 4"
+   runstring="srun -N $nodes -n 8 /users/brobey/hdf5proxy/hdf5proxy -size $dsize -nb_files 1"
    echo $runstring
-   echo -n "Data size is $dsize "
+   echo -n "Data size is $dsize Nodes is $nodes "
    eval "$runstring  |grep 'Elapsed time'"
-   dsize=$(( $dsize*2 ))
+   #dsize=$(( $dsize*2 ))
 done
 
