17f645975c790886f920cb90095c2a7dbb544afc
Bob Robey <brobey@lanl.gov>
2020-03-03 21:44:04 -0700

    Cleaning up hdf5 test

--- flecsi-hdf5proxy/jobtest_trinitite.sh
+++ flecsi-hdf5proxy/jobtest_trinitite.sh
@@ -14,33 +14,36 @@ module swap craype-haswell craype-mic-knl
 module load cray-hdf5-parallel/1.10.5.2
 module load cmake/3.16.2
 
-module list
+#module list
 
-SCRATCH_DIR=/lustre/ttscratch1/gshipman
-EXEC=/usr/projects/eap/users/gshipman/hdf5proxy/hdf5proxy
 MEM_FRACTION=1/4
+SCRATCH_DIR=/lustre/ttscratch1/${USER}
+if [ ${USER} == "gshipman" ];
+then
+   EXEC=/usr/projects/eap/users/gshipman/hdf5proxy/hdf5proxy
+else
+   EXEC=/users/${USER}/flecsi-incubator/flecsi-hdf5proxy/hdf5proxy
+fi
 
 rm -rf ${SCRATCH_DIR}/hdf5proxy
 mkdir ${SCRATCH_DIR}/hdf5proxy
 cd ${SCRATCH_DIR}/hdf5proxy
 
-echo "Memory per node is 96 GB. Writing out ${MEM_FRACTION} of node memory"
+memfracGB=`echo "scale=2; 96*${MEM_FRACTION}" | bc -l`
+echo "Memory per node is 96 GB. Writing out ${MEM_FRACTION} of node memory or ${memfracGB}"
 for ranks_per_node in 32 64
 do
    echo ""
    for nodes in 1 16 32 64 96
    do
-      dsizeGB=$(( nodes*96*${MEM_FRACTION} ))
+      dsizeGB=`echo "scale=2; ${nodes}*96*${MEM_FRACTION}" | bc -l`
       dsize=$(( nodes*96* 1024 * 1024 * 1024 *${MEM_FRACTION} ))
-      #dsize=`echo '${nodes}*96*${MEM_FRACTION}*1024*1024*1024' | bc -l`
       nfiles=$(( nodes/16 ))
       ranks=$(( ${ranks_per_node}*${nodes} ))
       if [ ${nfiles} -eq 0 ]; then  nfiles=1; fi
       printf "%-38s" "Data size is $dsizeGB GiB Nodes is $nodes "
       runstring="srun -N $nodes --ntasks-per-node $ranks_per_node ${EXEC} -size $dsize -nb_files $nfiles"
       echo $runstring
-      #echo -n "Data size is $dsizeGB GiB Nodes is $nodes	"
-      #eval "$runstring  |grep 'Elapsed time'"
       BATCH_JOB="job${nodes}_${ranks_per_node}"
       echo "#!/bin/bash -l"                              >  $BATCH_JOB
       echo "#SBATCH -N $nodes"                           >> $BATCH_JOB
@@ -49,18 +52,16 @@ do
       echo "#SBATCH -t 1:00:00"                          >> $BATCH_JOB
       echo "#SBATCH -J hdf$nodes_$ranks_per_node"        >> $BATCH_JOB
       echo "#SBATCH -p knl"                              >> $BATCH_JOB
-      echo "SCRATCH_DIR=${SCRATCH_DIR}"       >> $BATCH_JOB
-      echo "EXEC=${EXEC}"      >> $BATCH_JOB
-      echo "MEM_FRACTION=${MEM_FRACTION}"                           >> $BATCH_JOB
+      echo "SCRATCH_DIR=${SCRATCH_DIR}"                  >> $BATCH_JOB
+      echo "EXEC=${EXEC}"                                >> $BATCH_JOB
       echo "cd ${SCRATCH_DIR}/hdf5proxy"                 >> $BATCH_JOB
       echo "mkdir run${nodes}_${ranks_per_node}"         >> $BATCH_JOB
       echo "cd run${nodes}_${ranks_per_node}"            >> $BATCH_JOB
-      #echo runstring="srun -N $nodes --ntasks-per-node $ranks_per_node ${EXEC} -size $dsize -nb_files $nfiles" >> $BATCH_JOB
-      echo "echo \"$runstring \" "  >> $BATCH_JOB
-      echo "$runstring  "            >> $BATCH_JOB
+      echo "echo \"$runstring \" "                       >> $BATCH_JOB
+      echo "$runstring  "                                >> $BATCH_JOB
       echo $BATCH_JOB
       echo "==============="
-      cat $BATCH_JOB
+      #cat $BATCH_JOB
       echo ""
       sbatch < $BATCH_JOB
    done
